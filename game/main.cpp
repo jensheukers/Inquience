987ba851f3c89e9f1a78a0eecad3782ea610586f
Jens Heukers <jens.heukers@hotmail.com>
2019-05-02 14:26:37 +0200

    Reset main

--- game/main.cpp
+++ game/main.cpp
@@ -1,15 +1,12 @@
 // Main entry point for Dustville.
 //
-// Version: 1/5/2019
+// Version: 2/5/2019
 //
 // Copyright (C) Jens Heukers - All Rights Reserved
 // Unauthorized copying of this file, via any medium is strictly prohibited
 // Proprietary and confidential
 // Written by Jens Heukers, May 2019
 #include "../engine/core.h"
-#include "../engine/input.h"
-#include "../engine/scenemanager.h"
-#include "unit.h"
 
 //Define default tilemap scale
 #define DEFAULT_TILEMAP_TILE_SCALE 32 
@@ -17,21 +14,8 @@
 int main(int argc, char * argv[]) {
 	Core::Initialize(argc, argv);
 
-	SceneManager::SetActiveScene(new Scene());
-	SceneManager::GetActiveScene()->SetActiveCamera(new Camera());
-
-	Unit* unit = new Unit();
-	SceneManager::GetActiveScene()->AddChild(unit);
-
 	while (Core::IsActive()) {
 		Core::Update(); // Handle updates
-
-		float angle = unit->GetPosition().Angle(Input::GetMousePosition());
-		Debug::Log(std::to_string(Vec2::RadToDeg(angle)));
-
-		if (Input::GetButtonDown(BUTTONCODE_LEFT)) {
-			unit->SetDestination(Input::GetMousePosition());
-		}
 	}
 
 	Core::Destroy();
