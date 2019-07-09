ef0f9efb235e5544bdb1a1389f9e56c50695680b
Jens Heukers <jens.heukers@hotmail.com>
2019-07-09 23:07:46 +0200

    Work on moving units

--- game/client.cpp
+++ game/client.cpp
@@ -13,6 +13,7 @@
 
 //Include engine related headers
 #include "../engine/debug.h"
+#include "../engine/input.h"
 
 //Include game related headers
 #include "unit.h"
@@ -35,12 +36,62 @@ Client::Client() {
 
 void Client::Start() {
 	//Load the main menu first
-	instance->mainMenu = new MainMenu();
+	//instance->mainMenu = new MainMenu();
 	
 	//Set active
-	SceneManager::SetActiveScene(instance->mainMenu);
+	//SceneManager::SetActiveScene(instance->mainMenu);
+
+	Scene* scene = new Scene();
+	scene->SetActiveCamera(new Camera());
+	SceneManager::SetActiveScene(scene);
+
+	Unit* unit = new Unit();
+	Unit* unit2 = new Unit();
+
+	unit2->localPosition = Vec2(500, 300);
+
+	scene->AddChild(unit);
+	scene->AddChild(unit2);
+
+	instance->units.push_back(unit);
+	instance->units.push_back(unit2);
 }
 
 void Client::Update() {
 	//Updates
+
+	//In-Game Updates
+	if (!instance->inSession) return; // If user is not playing a session we will return here
+
+	//If Right Mouse Button is clicked, units within X pixels of the click location, will be added to selectedUnits vector
+	//If there are no units found, we will unselect all units
+	bool _found = false;
+	if (Input::GetButtonDown(BUTTONCODE_RIGHT)) {
+		for each (Unit* u in instance->units) {
+			if (Vec2::Distance(u->GetPosition(), Input::GetMousePosition() + SceneManager::GetActiveScene()->GetActiveCamera()->GetPosition()) < UNIT_SELECT_RANGE) {
+				//Check if control is not pressed, we unselect all selected units
+				if (!Input::GetKeyDown(KEYCODE_LEFT_CONTROL)) {
+					for (unsigned i = 0; i < instance->selectedUnits.size(); i++) {
+						instance->selectedUnits.erase(instance->selectedUnits.begin() + i);
+					}
+				}
+
+				instance->selectedUnits.push_back(u);
+				_found = true;
+			}
+		}
+
+		if (!_found) {
+			for (unsigned i = 0; i < instance->selectedUnits.size(); i++) {
+				instance->selectedUnits.erase(instance->selectedUnits.begin() + i);
+			}
+		}
+	}
+
+	//If Left Mouse button is clicked, all selected units will move to that position on the screen
+	if (Input::GetButtonDown(BUTTONCODE_LEFT)) {
+		for each (Unit* u in instance->selectedUnits) {
+			u->SetDestination(Input::GetMousePosition() + SceneManager::GetActiveScene()->GetActiveCamera()->GetPosition());
+		}
+	}
 }
