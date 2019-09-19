1ea4b0f13bd65c796fb11bde10a3d500242e98eb
Jens Heukers <jens.heukers@hotmail.com>
2019-09-19 17:05:13 +0200

    Master branch 19/9/2019

--- game/main.cpp
+++ game/main.cpp
@@ -1,11 +1,4 @@
 // Main entry point.
-//
-// Version: 8/8/2019
-//
-// Copyright (C) Jens Heukers - All Rights Reserved
-// Unauthorized copying of this file, via any medium is strictly prohibited
-// Proprietary and confidential
-// Written by Jens Heukers, August 2019
 #include "../engine/core.h"
 #include "../engine/input.h"
 
