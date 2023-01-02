0371405fee06409f67d6647060389375d8ff517e
Jens Heukers <jens.heukers@hotmail.com>
2023-01-02 17:23:56 +0100

    Updated cmakelists and removed editorwindows

--- worldbuilder/editorwindows/wb_componenteditorwindow.cpp
+++
@@ -1,7 +0,0 @@
-// Main entry point.
-//
-// Copyright (C) Jens Heukers - All Rights Reserved
-// Unauthorized copying of this file, via any medium is strictly prohibited
-// Proprietary and confidential
-// Written by Jens Heukers
-#include "wb_componenteditorwindow.h"
